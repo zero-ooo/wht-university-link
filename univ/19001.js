e270a065db3031ef2ccbd9fd978c2302a9007e23
zero-ooo <46119182+zero-ooo@users.noreply.github.com>
2018-12-25 11:52:50 +0800

    Create 19001.js

---
+++ univ/19001.js
@@ -0,0 +1,13 @@
+var univ_list = {
+    school: "重庆大学",                        // 学校名称
+    id: "19001",                               // 学校 ID
+    link: [ {
+        name: "重大教务处",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://www.jwc.cqu.edu.cn/"   // 链接地址
+    }, {
+        ...
+    }, {
+        name: "重大主页",                        // 链接数量小于或等于7个
+        url: "http://www.cqu.edu.cn/"
+    } ]
+};
